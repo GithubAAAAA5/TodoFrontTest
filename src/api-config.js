8c8f1f8cc7a49a9e7a7b443b31b0510018e69e8b
GithubAAAAA5 <gusrhks712@gmail.com>
2024-08-06 07:22:21 +0000

    first commit

--- src/api-config.js
+++ src/api-config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname;
 if(hostname === "localhost") {
   backendHost = "http://localhost:8080";
 }else {
-  backendHost = "https://dev.lhg712.p-e.kr";
+  backendHost = "http://todo-backend-test-env.eba-5iqykwjm.ap-northeast-2.elasticbeanstalk.com";
 }
 
 export const API_BASE_URL = `${backendHost}`;
